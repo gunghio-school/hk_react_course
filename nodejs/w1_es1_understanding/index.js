b4257e0f85c4158d5ee5f25310f7f32ab47052de
Olmo <olmo.barberis@supsi.ch>
2020-09-08 11:06:41 +0200

    Simple node example

--- nodejs/w1_es1_understanding/index.js
+++ nodejs/w1_es1_understanding/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const rect = {
   perimeter: (x, y) => 2 * (x + y),
   area: (x, y) => x * y,
@@ -5,4 +6,16 @@ const rect = {
 
 function solveRect(l, b) {
   console.log(`Solving for rectangle with l = ${l} and b = ${b}`);
+
+  if (l <= 0 || b <= 0) {
+    console.log(`Rectangle dimensions should be greater than zero:  l = ${l},  and b = ${b}`);
+  } else {
+    console.log(`The area of the rectangle is ${rect.area(l, b)}`);
+    console.log(`The perimeter of the rectangle is ${rect.perimeter(l, b)}`);
+  }
 }
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);

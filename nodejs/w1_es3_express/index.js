6528e80ff275229c646682854271cad057d07a2d
Olmo <olmo.barberis@supsi.ch>
2020-09-10 22:03:15 +0200

    expressrouter

--- nodejs/w1_es3_express/index.js
+++ nodejs/w1_es3_express/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const dishRouter = require('./router/dishRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -12,48 +13,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  next();
-});
-
-app.get('/dishes', (req, res) => {
-  res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res) => {
-  res.end(`Will add the dish: ${req.body.name} with details: ${req.body.description}`);
-});
-
-app.put('/dishes', (req, res) => {
-  res.statusCode = 403;
-  res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res) => {
-  res.end('Deleting all dishes');
-});
-
-app.get('/dishes/:dishId', (req, res) => {
-  res.end(`Will send details of the dish: ${req.params.dishId} to you!`);
-});
-
-app.post('/dishes/:dishId', (req, res) => {
-  res.statusCode = 403;
-  res.end(`POST operation not supported on /dishes/${req.params.dishId}`);
-});
-
-app.put('/dishes/:dishId', (req, res) => {
-  res.write(`Updating the dish: ${req.params.dishId}\n`);
-  res.end(`Will update the dish: ${req.body.name
-  } with details: ${req.body.description}`);
-});
-
-app.delete('/dishes/:dishId', (req, res) => {
-  res.end(`Deleting dish: ${req.params.dishId}`);
-});
-
+app.use('/dishes', dishRouter);
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res) => {
   console.log(req.headers);

77103ee852a355c880dd8cac84d1a3030cf84341
Olmo Barberis <olmo.barberis@supsi.ch>
2021-03-08 14:12:41 +0100

    several exercises

---
+++ nodejs/w4_es4_oauth/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  secretKey: '12345-67890-09876-54321',
+  mongoUrl:
+    'mongodb+srv://gunghio:NNTYh5e6V77YS4Ng@gunghiocluster.mlsca.mongodb.net/hk_course?retryWrites=true&w=majority',
+  facebook: {
+    clientId: 'Your Client App ID',
+    clientSecret: 'Your Client App Secret',
+  },
+};
